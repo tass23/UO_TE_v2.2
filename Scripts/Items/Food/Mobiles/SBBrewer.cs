e1cd549bd01b505cfecfb4aef5e48f777e6d7c28
Raist <raist@uoexpanse.com>
2023-03-18 12:26:08 -0400

    Updated Spawn file
    DeadlyEvil spawn file re-created and working. Previously Erik the Bright Red did not properly mark the shield as received, but players could still complete the main quest chain.
    Updated the Brewer file for better listings during sales transactions.

--- Scripts/Items/Food/Mobiles/SBBrewer.cs
+++ Scripts/Items/Food/Mobiles/SBBrewer.cs
@@ -20,21 +20,20 @@ namespace Server.Mobiles
 			public InternalBuyInfo()
 			{
 				Add( new GenericBuyInfo( typeof( Keg ), 50, 20, 0xE7F, 0 ) );
-				Add( new GenericBuyInfo( typeof( BrewersTools ), 500, 20, 0x1EBC, 0 ) );
-				Add( new GenericBuyInfo( typeof( BreweryLabelMaker ), 500, 20, 0xFBF, 0 ) );
-				Add( new GenericBuyInfo( typeof( Malt ), 10, 20, 0x103D, 0 ) );
-				Add( new GenericBuyInfo( typeof( Barley ), 20, 20, 0x103F, 0 ) );
-				Add( new GenericBuyInfo( typeof( BrewersYeast ), 20, 20, 0x103F, 0 ) );
-				Add( new GenericBuyInfo( typeof( EmptyAleBottle ), 10, 20, 0x99B, 0 ) );
-				Add( new GenericBuyInfo( typeof( EmptyMeadBottle ), 10, 20, 0x99B, 0 ) );
-				Add( new GenericBuyInfo( typeof( EmptyJug ), 10, 20, 0x9C8, 0 ) );
-				Add( new GenericBuyInfo( "Vineyard Ground Deed", typeof( VinyardGroundAddonDeed ), 100000, 5, 0x14F0, 0 ) );
-				Add( new GenericBuyInfo( typeof( WinecraftersTools ), 5000, 5, 0xF00, 0 ) );
-				Add( new GenericBuyInfo( "Vineyard Label Maker", typeof( VinyardLabelMaker ), 1000, 5, 0xFC0, 0 ) );
-				Add( new GenericBuyInfo( typeof( WinecrafterSugar ), 500, 20, 4165, 0 ) );
-				Add( new GenericBuyInfo( typeof( WinecrafterYeast ), 500, 20, 4165, 0 ) );
-				Add( new GenericBuyInfo( typeof( GrapevinePlacementTool ), 10000, 5, 0xD1A, 0 ) );
+				Add( new GenericBuyInfo( typeof( Malt ), 100, 20, 0x103D, 0 ) );
+				Add( new GenericBuyInfo( typeof( Barley ), 100, 20, 0x103F, 0 ) );
+				Add( new GenericBuyInfo( typeof( BrewersYeast ), 200, 20, 0x103F, 0 ) );
+				Add( new GenericBuyInfo( typeof( BagOfSugar ), 200, 20, 4165, 0 ) );
+				Add( new GenericBuyInfo( typeof( EmptyAleBottle ), 5, 20, 0x99B, 0 ) );
+				Add( new GenericBuyInfo( typeof( EmptyMeadBottle ), 5, 20, 0x99B, 0 ) );
 				Add( new GenericBuyInfo( typeof( EmptyWineBottle ), 5, 20, 0xF0E, 0 ) );
+				Add( new GenericBuyInfo( typeof( EmptyJug ), 10, 20, 0x9C8, 0 ) );
+				Add( new GenericBuyInfo( typeof( WinecraftersTools ), 5000, 5, 0xF00, 0 ) );
+				Add( new GenericBuyInfo( typeof( BrewersTools ), 5000, 20, 0x1EBC, 0 ) );
+				Add( new GenericBuyInfo( "Vineyard Label", typeof( VinyardLabelMaker ), 1000, 5, 0xFC0, 0 ) );
+				Add( new GenericBuyInfo( typeof( BreweryLabelMaker ), 1000, 20, 0xFBF, 0 ) );
+				Add( new GenericBuyInfo( typeof( GrapevinePlacementTool ), 10000, 5, 0xD1A, 0 ) );
+				Add( new GenericBuyInfo( "Vineyard Deed", typeof( VinyardGroundAddonDeed ), 100000, 5, 0x14F0, 0 ) );
 			}
 		}
 
